5929c87d0e0039ca11f1921223c1b8f14c46bb52
Anatoly Holovko <anatolyholovko@gmail.com>
2015-06-11 19:12:03 +0300

    страница 53

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Anatoly on 11.06.2015.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTruwQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTruwQuestion = trueQuestion;
+    }
+}
